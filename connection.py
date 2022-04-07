854b7fd06bb56f678710288db0c16d0f9e26948b
a-codeAday <97583579+a-codeAday@users.noreply.github.com>
2022-04-07 11:07:42 +0800

    Add files via upload

--- connection.py
+++ connection.py
@@ -5,8 +5,8 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 def connect():
-    return pymysql.connect(host = config['pymysql']['host'],
-                           user = config['pymysql']['user'],
-                           password = config['pymysql']['password'],
-                           database = config['pymysql']['database'],
-                           port = config['pymysql'].getint('port'))
+    return pymysql.connect(host = config['PYMYSQL']['PYMYSQL_HOST'],
+                           user = config['PYMYSQL']['PYMYSQL_USER'],
+                           password = config['PYMYSQL']['PYMYSQL_PASSWORD'],
+                           database = config['PYMYSQL']['PYMYSQL_DB_NAME'],
+                           port = config['PYMYSQL'].getint('PYMYSQL_PORT'))
