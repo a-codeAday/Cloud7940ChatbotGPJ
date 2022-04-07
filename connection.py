8af43921ecd43af0292660ff19c93b50a0da03ff
a-codeAday <97583579+a-codeAday@users.noreply.github.com>
2022-04-07 13:07:00 +0800

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,8 +5,8 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 def connect():
-    return pymysql.connect(host = os.environ['PYMYSQL']['PYMYSQL_HOST'],
-                           user = os.environ['PYMYSQL']['PYMYSQL_USER'],
-                           password = os.environ['PYMYSQL']['PYMYSQL_PASSWORD'],
-                           database = os.environ['PYMYSQL']['PYMYSQL_DB_NAME'],
-                           port = os.environ['PYMYSQL'].getint('PYMYSQL_PORT'))
+    return pymysql.connect(host = config['PYMYSQL']['PYMYSQL_HOST'],
+                           user = config['PYMYSQL']['PYMYSQL_USER'],
+                           password = config['PYMYSQL']['PYMYSQL_PASSWORD'],
+                           database = config['PYMYSQL']['PYMYSQL_DB_NAME'],
+                           port = config['PYMYSQL'].getint('PYMYSQL_PORT'))
