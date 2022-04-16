13a5d95c37fcba6e46d6bce3a91e799256f8ff57
a-code-a-day <21463417@life.hkbu.edu.hk>
2022-04-16 11:27:57 +0800

    push heroku

--- connection.py
+++ connection.py
@@ -10,4 +10,4 @@ def connect():
                            user = os.environ['PYMYSQL_USER'],
                            password = os.environ['PYMYSQL_PASSWORD'],
                            database = os.environ['PYMYSQL_DB_NAME'],
-                           port = os.environ['PYMYSQL_PORT'])
+                           port = int(os.environ['PYMYSQL_PORT']))
