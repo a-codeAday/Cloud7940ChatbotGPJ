f827435cf66747370256c90e1efcac12e18ae946
YLam <97583579+a-codeAday@users.noreply.github.com>
2022-04-19 10:26:13 +0800

    Update chatbot_spoon_wine.py

--- chatbot_spoon_wine.py
+++ chatbot_spoon_wine.py
@@ -9,15 +9,10 @@ import os
 import logging
 
 import random
+
+#create connection to mysqldb
 import connection
 conn = connection.connect()
-#create connection to mysqldb
-#conn = pymysql.connect(host = os.environ['PYMYSQL_HOST'],
-#                           user = os.environ['PYMYSQL_USER'],
-#                           password = os.environ['PYMYSQL_PASSWORD'],
-#                           database = os.environ['PYMYSQL_DB_NAME'],
-#                           port = int(os.environ['PYMYSQL_PORT']))
-
 mycursor = conn.cursor()
 
 from spoonacular import API
